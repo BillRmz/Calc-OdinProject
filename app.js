0877d08975a9d5022cf7d05b6e54057fffd634c9
Bill Ramírez <bilyalon@gmail.com>
2022-05-11 20:34:33 -0600

    RPS Almost Done!

--- app.js
+++ app.js
@@ -1,1 +1,54 @@
-console.log ('Hello World');
+//This Function gets the computer's selection
+const computerplay = function () {
+  let arr = ["rock", "paper", "scissors"];
+  let select = Math.floor(Math.random() * arr.length); // Operacion que da un numero ramdon
+  return arr[select];
+};
+
+//This function plays a single round of Rock Paper Scissors.
+const playRound = function (pcSelection, plyrSelection) {
+  let pcSelect = pcSelection;
+  let playerSelect = plyrSelection.toString().toLowerCase();
+
+  if (playerSelect === "rock" && pcSelect === "scissors") {
+    return 1;
+  } else if (playerSelect === "paper" && pcSelect === "rock") {
+    return 1;
+  } else if (playerSelect === "scissor" && pcSelect === "paper") {
+    return 1;
+  } else if (playerSelect === pcSelect) {
+    return 2;
+  } else {
+    return 0;
+  }
+};
+
+const game = function () {
+  let pcCount = 0;
+  let plCount = 0;
+
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt("Please enter 'Rock' 'Paper' or 'Scissors'");
+    let pcSelection = computerplay().toString();
+
+    let reg = playRound(playerSelection, pcSelection);
+    console.log(reg);
+
+    if (reg === 1) {
+      plCount++;
+      console.log(`You win! \n ${playerSelection}  - PC chose ${pcSelection} `);
+    } else if (reg === 0) {
+      pcCount++;
+      console.log(` You Lost! \n  ${playerSelection} - PC chose ${pcSelection}`);
+    }else if (reg === 2){
+        console.log('TIE!')
+    }
+  }
+  let finalScore = `Player ${plCount} vs PC ${pcCount}`;
+   console.log(finalScore);
+  return finalScore;
+}
+
+
+
+game(); 
