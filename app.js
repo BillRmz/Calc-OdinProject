d35fc36e5e2e7aab3dcd61e2800f0157ec6613ed
Bill Ramírez <bilyalon@gmail.com>
2022-05-27 23:44:53 -0600

    User Interface

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+let playerScore = 0;
+let pcScore = 0;
+
+let player = document.querySelector("#playerScore");
+let pc = document.querySelector("#pcScore");
+
+player.textContent = playerScore.toString();
+pc.textContent = pcScore.toString();
+
+const buttons = document.querySelectorAll(".button");
+
+function disableButtons() {
+  buttons.forEach((elem) => {
+    elem.disabled = true;
+  });
+}
+
 //This Function gets the computer's selection
 const computerplay = function () {
   let arr = ["rock", "paper", "scissors"];
@@ -5,15 +22,30 @@ const computerplay = function () {
   return arr[select];
 };
 
+
+const emojiSelector = () => {
+  let arr = ["🥳", "👏", "🏆", "🙌🏻","🤘🏻","🎉","🎊"];
+  let select = Math.floor(Math.random() * arr.length); // gets a ramdon number
+  return arr[select];
+}
+
+const pcEmojiSelector = () => {
+  let arr = ["😆", "🤣", "😂", "💀","😬","🤪","🤭"];
+  let select = Math.floor(Math.random() * arr.length); // gets a ramdon number
+  return arr[select];
+}
+
+
+
 const playRound = function (pcSelection, plyrSelection) {
   let pcSelect = pcSelection;
   let playerSelect = plyrSelection.toString().toLowerCase();
 
-  if (playerSelect === "rock" && pcSelect === "scissors") {
-    return 1;
-  } else if (playerSelect === "paper" && pcSelect === "rock") {
-    return 1;
-  } else if (playerSelect === "scissor" && pcSelect === "paper") {
+  if (
+    (playerSelect === "rock" && pcSelect === "scissors") ||
+    (playerSelect === "paper" && pcSelect === "rock") ||
+    (playerSelect === "scissor" && pcSelect === "paper")
+  ) {
     return 1;
   } else if (playerSelect === pcSelect) {
     return 2;
@@ -22,33 +54,80 @@ const playRound = function (pcSelection, plyrSelection) {
   }
 };
 
-const game = function () {
-  //counter for score
-  let pcCount = 0;
-  let plCount = 0;
-  // this loop repeat 5 times, get the user and call the playRound Function.
-  for (let i = 0; i < 5; i++) {
-    let playerSelection = prompt("Please enter 'Rock' 'Paper' or 'Scissors'");
-    let pcSelection = computerplay().toString();
-  
-    let reg = playRound(pcSelection,playerSelection);
-    console.log(reg);
+const btnRefresh = function () {
+  const refresh = document.querySelector("#refresh");
+  const btnRefresh = document.createElement("a");
+  btnRefresh.textContent = "Play again?";
+  btnRefresh.style.border = "solid; color: black; 2px;";
+  btnRefresh.style.backgroundColor = "rgb(0, 38, 153)";
+  btnRefresh.style.border = "2px solid Black";
+  btnRefresh.style.padding = "1rem";
+  btnRefresh.style.borderRadius = "20px";
+  btnRefresh.style.textDecoration = "none";
+  btnRefresh.style.color = "white";
+  btnRefresh.style.cursor = "pointer";
+  btnRefresh.style.fontWeight = "bold";
 
-    if (reg === 1) {
-      plCount++;
-      console.log(`You win! \n ${playerSelection}  - PC chose ${pcSelection} `);
-    } else if (reg === 0) {
-      pcCount++;
-      console.log( ` You Lost! \n  ${playerSelection} - PC chose ${pcSelection}`);
-    } else if (reg === 2) {
-      console.log("TIE!");
-      i--;
-    }
-    console.log(playerSelection);
-    console.log(pcSelection);
-  }
-  let finalScore = `Player ${plCount} vs PC ${pcCount}`;
-  console.log(finalScore);
+  btnRefresh.setAttribute("href", "javascript:location.reload()");
+  refresh.appendChild(btnRefresh);
 };
 
-game();
+const game = function (selection) {
+  let text = document.querySelector("#text");
+  let playerSelection = selection;
+  let pcSelection = computerplay().toString();
+
+  let pcOutput = document.querySelector("#pcOutput");
+  pcOutput.textContent = pcSelection.toString();
+
+  let reg = playRound(pcSelection, playerSelection);
+
+  if (reg === 1) {
+    text.textContent = "You won!";
+    text.style.color = "green";
+    playerScore += 1;
+
+    if (playerScore == 5) {
+      text.textContent = `** You beat PC!! ${emojiSelector()} **` ;
+      disableButtons();
+      btnRefresh();
+    }
+  } else if (reg === 0) {
+    text.textContent = "You Loose!";
+    text.style.color = "red";
+    pcScore += 1;
+
+    if (pcScore == 5) {
+      text.textContent =  `** PC beats you!! ${pcEmojiSelector()} **`;
+      disableButtons();
+      btnRefresh();
+    }
+  } else if (reg === 2) {
+    text.textContent = "Tie";
+    text.style.color = "orange";
+  }
+  player.textContent = playerScore.toString();
+  pc.textContent = pcScore.toString();
+};
+
+const playerOutput = document.querySelector("#playerOutput");
+const pcOutput = document.querySelector("#pcOutput");
+
+const btnRock = document.querySelector("#rock");
+const btnPaper = document.querySelector("#paper");
+const btnScissors = document.querySelector("#scissors");
+
+btnRock.addEventListener("click", function (e) {
+  playerOutput.textContent = "Rock";
+  game("rock");
+});
+
+btnPaper.addEventListener("click", function (e) {
+  playerOutput.textContent = "Paper";
+  game("paper");
+});
+
+btnScissors.addEventListener("click", function (e) {
+  playerOutput.textContent = "Scissors";
+  game("scissors");
+});
